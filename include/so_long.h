640f7c713fd5b1dd20a8ee95a9e3d1d25ac381da
Luiza Kormann <109827640+luizakormann@users.noreply.github.com>
2025-03-31 22:15:35 -0300

    inital structs

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: luiza <luiza@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 19:35:19 by luiza             #+#    #+#             */
-/*   Updated: 2025/03/31 20:02:58 by luiza            ###   ########.fr       */
+/*   Updated: 2025/03/31 22:14:39 by luiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,33 @@
 # define SO_LONG_H
 # define WIDTH_TILE 32
 # define HEIGHT_TILE 32
-# define WIDTH 1920
-# define HEIGHT 1080
+# define WIDTH 1280
+# define HEIGHT 720
 
-# include "../lib/MLX42/include/MLX42/MLX42.h"
+# include "../minilibx-linux/mlx.h"
+# include "../minilibx-linux/mlx_int.h"
 # include "../libft/include/libft.h"
+# include <fcntl.h>
+
+typedef struct s_map
+{
+	char	**grid;
+	int		width;
+	int		height;
+}	t_map;
+
+typedef struct s_player
+{
+	int	x;
+	int	y;
+}	t_player;
+
+typedef struct s_game
+{
+	void		*mlx;
+	void		*win;
+	t_map		map;
+	t_player	player;
+}	t_game;
 
 #endif
